0f8554c8b9c0130833fbab07d69e41ba1e609e5d
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-12-13 12:37:58 +0100

    images added dist

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,39 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-export default defineConfig({
-  appType: "mpa",
-  base: "",
-  build: {
-    target: "esnext",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "./auth/login/index.html"),
-        register: resolve(__dirname, "./auth/register/index.html"),
-        profile: resolve(__dirname, "./profil/index.html"),
-        createListing: resolve(__dirname, "./lage/index.html"),
-        editListing: resolve(__dirname, "./rediger/index.html"),
-        singleListing: resolve(__dirname, "./annonse/index.html"),
-        updateProfile: resolve(__dirname, "./oppdater-profil/index.html"),
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import("vite-plugin-static-copy");
+
+  return {
+    appType: "mpa",
+    base: "",
+    build: {
+      target: "esnext",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "./index.html"),
+          login: resolve(__dirname, "./auth/login/index.html"),
+          register: resolve(__dirname, "./auth/register/index.html"),
+          profile: resolve(__dirname, "./profil/index.html"),
+          createListing: resolve(__dirname, "./lage/index.html"),
+          editListing: resolve(__dirname, "./rediger/index.html"),
+          singleListing: resolve(__dirname, "./annonse/index.html"),
+          updateProfile: resolve(__dirname, "./oppdater-profil/index.html"),
+        },
       },
     },
-  },
-  define: {
-    "import.meta.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
-  },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'images', 
+            dest: 'images' 
+          }
+        ]
+      })
+    ],
+    define: {
+      "import.meta.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
+    },
+  };
 });
