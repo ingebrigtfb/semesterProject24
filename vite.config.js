167e5265d09dabfedb46055eaa46b639bd72fd10
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-12-13 10:23:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Your Vite configuration
+  appType: "mpa",
+  base: "",
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        createListing: resolve(__dirname, "./listing/create/index.html"),
+        editListing: resolve(__dirname, "./listing/edit/index.html"),
+        singleListing: resolve(__dirname, "./listing/single/index.html"),
+        updateProfile: resolve(__dirname, "./profile/update/index.html"),
+      },
+    },
+  },
 });
